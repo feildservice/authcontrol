4e8a5caa71ea2a7e4be463afd07cca6048e6c9b0
solmon <solmon@gmail.com>
2024-10-22 22:20:52 +0530

    checking

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -7,4 +7,5 @@ export default async function auth(req: NextApiRequest, res: NextApiResponse) {
   const authOptions = getAuthOptions(req, res);
 
   return await NextAuth(req, res, authOptions);
+  
 }
